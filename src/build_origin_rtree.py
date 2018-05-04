8dde5e9e7d4480acec30fd4989a3162292149e7b
WuZhuoran <munoliver007@gmail.com>
2018-05-04 17:03:27 -0400

    style: add report image

--- src/build_origin_rtree.py
+++
@@ -1,7 +0,0 @@
-from collections import defaultdict
-from math import sqrt
-import rtree.index
-
-points = [(5, 4), (3, 1), (6, 3), (2, 8), (7, 8), (8, 1), (2, 3), (0, 4), (3, 7), (6, 4)]
-
-idx = rtree.index.Rtree()
